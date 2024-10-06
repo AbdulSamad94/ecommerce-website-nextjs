c5b9e91e18986adceeb6f39386fc706689fc95f7
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2024-10-06 17:36:37 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 
 const data = [
   {
-    number: 1300,
+    number: 200,
     text: "International Brands",
   },
   {
@@ -19,28 +19,30 @@ const data = [
 ];
 const Hero = () => {
   return (
-    <section className="w-full h-full bg-zinc-100 pt-20">
-      <div className="flex justify-between items-center">
-        <div className="w-2/5 ml-10 flex flex-col">
-          <h1 className="uppercase text-5xl text-left font-bold mb-8">
+    <section className="w-full h-full bg-zinc-100 pt-8 md:pt-20">
+      <div className="flex lg:justify-between flex-col lg:flex-row items-center">
+        <div className="md:w-2/5 px-3 md:ml-10 md:px-0 flex flex-col">
+          <h1 className="uppercase text-4xl md:text-5xl w-full text-left font-bold mb-4 md:mb-8">
             find clothes that matches your styles
           </h1>
-          <p className="text-zinc-500 text-[16px] mb-8">
+          <p className="text-zinc-500 text-[16px] mb-5 md:mb-8">
             Browse through our diverse range of meticulously crafted garments,
             designed to bring out your individuality and cater to your sense of
             style.
           </p>
-          <button className="bg-black text-white py-3 px-5 w-48 h-12 rounded-full flex items-center justify-center mb-12">
+          <button className="bg-black text-white py-3 px-5 w-full md:w-48 h-12 rounded-full flex items-center justify-center mb-12">
             Shop Now
           </button>
-          <div className="flex gap-x-10 mb-20">
+          <div className="flex gap-10 mb-20 flex-wrap justify-center md:justify-normal">
             {data.map((item, index) => (
               <div key={index}>
                 <div className="text-4xl te font-bold mb-2">
                   <CountUp end={item.number} duration={6} />
                   {"+"}
                 </div>
-                <div className="text-[16px] text-zinc-500 ">{item.text}</div>
+                <div className="text-sm md:text-[16px] text-zinc-500 ">
+                  {item.text}
+                </div>
               </div>
             ))}
           </div>
@@ -51,7 +53,7 @@ const Hero = () => {
             alt="star"
             width={80}
             height={80}
-            className="absolute -left-20 top-1/4"
+            className="absolute md:-left-20 top-1/4"
           />
           <Image
             src="/hero/Vector.png"
@@ -65,10 +67,11 @@ const Hero = () => {
             width={560}
             height={560}
             alt="hero-bg"
+            className="lg:w-[560px] lg:h-[560px] w-[400px] h-[400px] "
           />
         </div>
       </div>
-      <div className="w-full bg-black h-20 mb-10 flex justify-around items-center">
+      <div className="w-full bg-black h-32 md:h-20 mb-10 flex justify-center md:justify-around items-center gap-x-5 flex-wrap">
         <Image
           src="/hero/one-text.png"
           alt="text-logo"
